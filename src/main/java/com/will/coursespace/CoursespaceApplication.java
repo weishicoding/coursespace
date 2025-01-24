fb18c407bd845bc54eaface57cd07472adecb771
weishicoding <sanwind0305@gmail.com>
2025-01-24 20:37:51 +0200

    Create a new  project

---
+++ src/main/java/com/will/coursespace/CoursespaceApplication.java
@@ -0,0 +1,13 @@
+package com.will.coursespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/coursespace/CoursespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.coursespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
