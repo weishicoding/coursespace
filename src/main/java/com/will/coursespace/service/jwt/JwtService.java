ccf738af240835cfd633f7bfcdb2daf71771a2ce
weishicoding <sanwind0305@gmail.com>
2025-02-16 18:18:10 +0200

    Fixing jwt authentication

--- src/main/java/com/will/coursespace/service/jwt/JwtService.java
+++ src/main/java/com/will/coursespace/service/jwt/JwtService.java
@@ -5,19 +5,13 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.micrometer.common.util.StringUtils;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -30,14 +24,11 @@ public class JwtService {
     @Value("${jwt.jwtExpirationInMs}")
     private Long jwtExpiration;
 
-    private final UserDetailsService userDetailsService;
-
     public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userDetails);
+        return createToken(userDetails);
     }
 
-    private String createToken(Map<String, Object> claims, UserDetails userDetails) {
+    private String createToken(UserDetails userDetails) {
         return Jwts.builder()
                 .claim("roles", userDetails.getAuthorities())
                 .setSubject(userDetails.getUsername())
@@ -45,17 +36,6 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
-    }
-
-    public String generateRefresh(Map<String, Objects> extraClaims, UserDetails userDetails) {
-        return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 604800000))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-                .compact();
-
     }
 
     private Key getSigningKey() {
@@ -67,22 +47,9 @@ public class JwtService {
         return extractClaim(token, Claims::getSubject);
     }
 
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
-    }
-
-    private Boolean isTokenInvalid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 
     private Claims extractAllClaims(String token) {
@@ -91,14 +58,5 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
-    }
-
-    public boolean validateToken(String token) {
-        String username = extractUsername(token);
-        if (StringUtils.isNotEmpty(username) && !isTokenExpired(token)){
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-            return isTokenInvalid(token, userDetails);
-        }
-        return false;
     }
 }
