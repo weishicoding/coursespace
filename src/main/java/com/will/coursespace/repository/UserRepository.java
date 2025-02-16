79db22bfea18b5d40e5fa3aa38c09516ff34d4be
weishicoding <sanwind0305@gmail.com>
2025-02-16 18:10:50 +0200

    Fixing jwt authentication

--- src/main/java/com/will/coursespace/repository/UserRepository.java
+++ src/main/java/com/will/coursespace/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.will.coursespace.repository;
 
 import com.will.coursespace.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
